e6ffb0312f609c7954f82b71933c1688334f4a8d
Nimasoy <nimarezaei.me@gmail.com>
2025-05-01 23:39:50 +0330

    modified constructor and changed some field types

--- Library.Domain/Entities/Reservation.cs
+++ Library.Domain/Entities/Reservation.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Library.Domain.Entities
+﻿namespace Library.Domain.Entities
 {
-    public class Reservation
+    public class Reservation(Book book)
     {
-        public int Id { get; set; }
-        public int BookId { get; set; }
-        public int UserId { get; set; }
-        public DateTime ReservedAt { get; set; }
-        public Book Book { get; set; }
+        public int Id { get; private set; }
+        public int BookId { get; private set; }
+        public int UserId { get; private set; }
+        public DateTime ReservedAt { get; private set; }
+        public Book Book { get; private set; } = book ?? throw new ArgumentNullException(nameof(book));
     }
 }
