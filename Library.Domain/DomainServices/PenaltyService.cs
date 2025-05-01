6a4d4ddd7b4a4aab8e69d22b34cb8b2e843bf9ee
Nimasoy <nimarezaei.me@gmail.com>
2025-05-02 02:00:23 +0330

    added PenaltyService

---
+++ Library.Domain/DomainServices/PenaltyService.cs
@@ -0,0 +1,35 @@
+﻿using Library.Domain.Entities;
+
+namespace Library.Domain.DomainServices
+{
+    public record PenaltyCalculatedEvent(int UserId, int BookId, int DaysOverdue, int TotalFine);
+
+    public class PenaltyService
+    {   
+        private readonly IDomainEventDispatcher _eventDispatcher;
+        public PenaltyService(IDomainEventDispatcher eventDispatcher)
+        {
+            _eventDispatcher = eventDispatcher;
+        }
+        private const int PenaltyAmountPerDay = 10;
+        public Dictionary<int, int> CalculatePenalty(User user)
+        {   
+            var penalties = new Dictionary<int, int>();
+
+            foreach (var record in user.Borrows)
+            {
+                if (record.ReturnedAt == null && record.DueAt < DateTime.UtcNow)
+                {
+                    int daysOverdue = (DateTime.UtcNow - record.DueAt).Days;
+                    int totalAmount = daysOverdue * PenaltyAmountPerDay;
+                    penalties[record.BookId] = totalAmount;
+
+                    _eventDispatcher.Dispatch(new PenaltyCalculatedEvent(
+                    user.Id, record.BookId, daysOverdue, totalAmount));
+                }
+            }
+            return penalties;
+        }
+    }
+
+}
