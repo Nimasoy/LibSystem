2f82998f9ff41edc8b5b6abc31f00b1ae144fac6
Nimasoy <nimarezaei.me@gmail.com>
2025-05-01 23:18:09 +0330

    added value objects for Category entity

---
+++ Library.Domain/ValueObjects/Category/CategoryName.cs
@@ -0,0 +1,13 @@
+﻿namespace Library.Domain.ValueObjects.Category
+{
+    public class CategoryName
+    {
+        public string Value { get;}
+        public CategoryName(string value)
+        {
+            if (string.IsNullOrEmpty(value)) throw new ArgumentException("Category name cannot be empty");
+
+            Value = value;
+        }
+    }
+}
