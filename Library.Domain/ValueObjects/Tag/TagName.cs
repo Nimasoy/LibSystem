7c0213ee616928e8d9ae39ee1a3790414497b9f1
Nimasoy <nimarezaei.me@gmail.com>
2025-05-01 23:47:21 +0330

    added value objects for Tag entity

---
+++ Library.Domain/ValueObjects/Tag/TagName.cs
@@ -0,0 +1,13 @@
+﻿namespace Library.Domain.ValueObjects.Tag
+{
+    public class TagName
+    {
+        public string Value { get; }
+        public TagName(string value)
+        {
+            if (string.IsNullOrEmpty(value)) throw new ArgumentException("Tag name cannot be empty");
+
+            Value = value;
+        }
+    }
+}
